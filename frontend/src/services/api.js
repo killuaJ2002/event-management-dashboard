22558c5a544d742eaeb38f9557cc70e9ada50efc
killuaJ2002 <kjcodes2002@gmail.com>
2025-07-02 16:20:16 +0530

    added render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://event-management-dashboard-neix.onrender.com/api",
 });
 
 // Add token to every request
